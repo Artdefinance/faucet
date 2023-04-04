50e7aafc1043060be63d5972f9c5cedef1ecebe3
gimeoni <wh.kim@artdefinance.io>
2023-04-04 18:35:07 +0900

    feat: [DEVTEAM-56] Apply reCAPTCHA

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -3,7 +3,7 @@ import styles from "@/components/Button/Button.module.css";
 const Button = (props) => {
   const { onClick, disabled, children } = props;
   return (
-    <button className={styles.button} onChange={onClick} disabled={disabled}>
+    <button className={styles.button} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,10 +1,12 @@
 import { useState, useMemo } from "react";
 import Head from "next/head";
 import { Lexend } from "next/font/google";
+import { ethers } from "ethers";
+import { ToastContainer, toast, Zoom } from "react-toastify";
+import axios from "axios";
 import styles from "@/styles/Home.module.css";
 import Input from "@/components/Input/Input";
 import Button from "@/components/Button/Button";
-import { ethers } from "ethers";
 
 const lexend = Lexend({
   subsets: ["latin"],
@@ -14,6 +16,7 @@ const lexend = Lexend({
 const Home = () => {
   const [address, setAddress] = useState("");
   const [touched, setTouched] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setAddress(e.target.value);
@@ -28,6 +31,59 @@ const Home = () => {
     return isAddress(address);
   }, [address]);
 
+  const reset = () => {
+    setAddress("");
+    setTouched(false);
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    grecaptcha.enterprise
+      .execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY, {
+        action: "login",
+      })
+      .then(function (token) {
+        axios
+          .post(`${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/faucets`, {
+            address,
+            captchaResponse: token,
+          })
+          .then((response) => {
+            console.log(`tx Hash : ${JSON.stringify(response.data)}`);
+            toast.success(`Faucet Success`, { transition: Zoom });
+            reset();
+            setLoading(false);
+          })
+          .catch((err) => {
+            let errText = "Faucet Fail";
+            if (typeof err.response == "undefined" || err.response == null) {
+              errText = "Unknown status";
+            } else {
+              switch (err.response.status) {
+                case 400:
+                  errText = "Invalid request";
+                  break;
+                case 403:
+                  errText = "Too many requests";
+                  break;
+                case 404:
+                  errText = "Cannot connect to server";
+                  break;
+                case 502:
+                case 503:
+                  errText = "Faucet service temporary unavailable";
+                  break;
+                default:
+                  errText = err.response.data || err.message;
+                  break;
+              }
+            }
+            toast.error(`${errText}`, { transition: Zoom });
+            setLoading(false);
+          });
+      });
+  };
+
   return (
     <>
       <Head>
@@ -37,20 +93,35 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={lexend.variable}>
-          <div className={styles.center}>
-            <h1>ADF Testnet Faucet</h1>
-            <Input
-              value={address}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              error={(!address || !isAddressValidate) && touched}
-              placeholder="Enter your address"
-            />
-            <Button disabled={!address || !isAddressValidate}>
-              Send me tokens !
-            </Button>
-            <div className={styles.footer}>© 2023 ART DE FINANACE</div>
+        <div className={`${styles.center} ${lexend.variable}`}>
+          <h1>ADF Testnet Faucet</h1>
+
+          <Input
+            value={address}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={(!address || !isAddressValidate) && touched}
+            placeholder="Enter your address"
+          />
+          <Button
+            disabled={!address || !isAddressValidate || loading}
+            onClick={handleSubmit}
+          >
+            Send me tokens !
+          </Button>
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+          <div className={styles.footer}>
+            © 2023 <b>Art de Finance</b>
           </div>
         </div>
       </main>
