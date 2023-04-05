5e6481d5f3cbf3666dd9d9497d48e0df789b1a37
gimeoni <wh.kim@artdefinance.io>
2023-04-05 11:43:50 +0900

    feat: [DEVTEAM-49] Change reCaptcha version from enterprise to v2

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,10 @@
-import { useState, useMemo } from "react";
+import React, { useState, useMemo, useRef } from "react";
 import Head from "next/head";
 import { Lexend } from "next/font/google";
 import { ethers } from "ethers";
 import { ToastContainer, toast, Zoom } from "react-toastify";
 import axios from "axios";
+import ReCAPTCHA from "react-google-recaptcha";
 import styles from "@/styles/Home.module.css";
 import Input from "@/components/Input/Input";
 import Button from "@/components/Button/Button";
@@ -17,6 +18,9 @@ const Home = () => {
   const [address, setAddress] = useState("");
   const [touched, setTouched] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [recaptcha, setRecaptcha] = useState(null);
+
+  const recaptchaRef = useRef(undefined);
 
   const handleChange = (e) => {
     setAddress(e.target.value);
@@ -34,53 +38,54 @@ const Home = () => {
   const reset = () => {
     setAddress("");
     setTouched(false);
+    setRecaptcha(null);
+    recaptchaRef.current.reset();
+  };
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptcha(value);
   };
 
   const handleSubmit = () => {
     setLoading(true);
-    grecaptcha.enterprise
-      .execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY, {
-        action: "login",
+
+    axios
+      .post(`${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/faucets`, {
+        address,
+        captchaResponse: recaptcha,
       })
-      .then(function (token) {
-        axios
-          .post(`${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/faucets`, {
-            address,
-            captchaResponse: token,
-          })
-          .then((response) => {
-            console.log(`tx Hash : ${JSON.stringify(response.data)}`);
-            toast.success(`Faucet Success`, { transition: Zoom });
-            reset();
-            setLoading(false);
-          })
-          .catch((err) => {
-            let errText = "Faucet Fail";
-            if (typeof err.response == "undefined" || err.response == null) {
-              errText = "Unknown status";
-            } else {
-              switch (err.response.status) {
-                case 400:
-                  errText = "Invalid request";
-                  break;
-                case 403:
-                  errText = "Too many requests";
-                  break;
-                case 404:
-                  errText = "Cannot connect to server";
-                  break;
-                case 502:
-                case 503:
-                  errText = "Faucet service temporary unavailable";
-                  break;
-                default:
-                  errText = err.response.data || err.message;
-                  break;
-              }
-            }
-            toast.error(`${errText}`, { transition: Zoom });
-            setLoading(false);
-          });
+      .then((response) => {
+        console.log(`tx Hash : ${JSON.stringify(response.data)}`);
+        toast.success(`Faucet Success`, { transition: Zoom });
+        reset();
+        setLoading(false);
+      })
+      .catch((err) => {
+        let errText = "Faucet Fail";
+        if (typeof err.response == "undefined" || err.response == null) {
+          errText = "Unknown status";
+        } else {
+          switch (err.response.status) {
+            case 400:
+              errText = "Invalid request";
+              break;
+            case 403:
+              errText = "Too many requests";
+              break;
+            case 404:
+              errText = "Cannot connect to server";
+              break;
+            case 502:
+            case 503:
+              errText = "Faucet service temporary unavailable";
+              break;
+            default:
+              errText = err.response.data || err.message;
+              break;
+          }
+        }
+        toast.error(`${errText}`, { transition: Zoom });
+        setLoading(false);
       });
   };
 
@@ -95,7 +100,11 @@ const Home = () => {
       <main className={styles.main}>
         <div className={`${styles.center} ${lexend.variable}`}>
           <h1>ADF Testnet Faucet</h1>
-
+          <ReCAPTCHA
+            sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY}
+            onChange={handleRecaptchaChange}
+            ref={recaptchaRef}
+          />
           <Input
             value={address}
             onChange={handleChange}
@@ -104,7 +113,7 @@ const Home = () => {
             placeholder="Enter your address"
           />
           <Button
-            disabled={!address || !isAddressValidate || loading}
+            disabled={!address || !isAddressValidate || !recaptcha || loading}
             onClick={handleSubmit}
           >
             Send me tokens !
