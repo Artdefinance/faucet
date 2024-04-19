1116d1e141b17012d3f3c432b6aef9a446931845
gimeoni <wh.kim@artdefinance.io>
2024-04-19 15:00:22 +0900

    fix: Replace seeding with launchpad

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -90,8 +90,8 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Artiside Seeding Testnet Faucet</title>
-        <meta name="description" content="Artiside Seeding Testnet Faucet" />
+        <title>Artiside Launchpad Testnet Faucet</title>
+        <meta name="description" content="Artiside Launchpad Testnet Faucet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -122,7 +122,7 @@ const Home = () => {
                 </Button>
               </div>
             ) : (
-              <div className={styles.seeding}>
+              <div className={styles.launchpad}>
                 <Link
                   href="https://artiside.testnet.artdefinance.io/launchpad"
                   target="_blank"
